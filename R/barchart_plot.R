a1806dac365d43788b679a1f82214e94d831cbed
ARUMHC <56126275+ARUMHC@users.noreply.github.com>
2021-07-19 16:04:14 +0200

    trying to make check run

--- R/barchart_plot.R
+++ R/barchart_plot.R
@@ -177,31 +177,31 @@ barchart_plot_normalized <- function(data, cat, series, series_labels){
 
 # test
 
-data <- data.frame(
-  city = c("Berlin", "Munich", "Cologne", "London", "Vienna", "Paris", "Zurich", "Rest"),
- value = c(1159, 795, 377, 345, 266,120,74,602),
-  products = c(538, 250, -75, -301,227,90, 40, 269),
-  services = c(621,545,-302, -44,39,30,34,333)
-)
-groups <- c("products")
+#data <- data.frame(
+#  city = c("Berlin", "Munich", "Cologne", "London", "Vienna", "Paris", "Zurich", "Rest"),
+# value = c(1159, 795, 377, 345, 266,120,74,602),
+#  products = c(538, 250, -75, -301,227,90, 40, 269),
+#  services = c(621,545,-302, -44,39,30,34,333)
+#)
+#groups <- c("products")
 
-df <- data.frame(
-  animal = c("cat", "doggo", "rabbito"),
-  hungry = c(7,5,9),
-  relaxed = c(3,4,5),
-  wounded = c(1,8,5)
-)
-srs <- c("hungry", "relaxed", "wounded")
-groups <- c("products")
-styles <- c(rep('actual',6), 'forecast', 'actual')
+#df <- data.frame(
+#  animal = c("cat", "doggo", "rabbito"),
+#  hungry = c(7,5,9),
+#  relaxed = c(3,4,5),
+#  wounded = c(1,8,5)
+#)
+#srs <- c("hungry", "relaxed", "wounded")
+#groups <- c("products")
+#styles <- c(rep('actual',6), 'forecast', 'actual')
 
-barchart_plot_index(
-  data,
-  cat = data$city,
-  groups,
-  index_val = 602,
-  series_labels = groups
-) %>% show()
-barchart_plot(data, data$city, groups, groups, styles = styles) %>% show()
+#barchart_plot_index(
+#  data,
+#  cat = data$city,
+#  groups,
+#  index_val = 602,
+#  series_labels = groups
+#) %>% show()
+#barchart_plot(data, data$city, groups, groups, styles = styles) %>% show()
 
 #barchart_plot_normalized(data, data$city, groups, groups) %>% show() # obwód maskującego prostokąta renderuje się tylko w Rstudio. Po zapisaniu do pliku wygląda na całkowicie biały
