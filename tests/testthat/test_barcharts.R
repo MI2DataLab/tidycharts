40c6ce8809a35a0ada4dbfd477d52a3f860b0789
SawickiBartosz <b.sawicki@student.mini.pw.edu.pl>
2021-07-20 10:39:14 +0200

    add barcharts tests

--- tests/testthat/test_barcharts.R
+++ tests/testthat/test_barcharts.R
@@ -1,6 +1,4 @@
-context("Test horizontal barchart plots")
-
-
+context("Horizontal barchart plots")
 
 data <- data.frame(
   city = c(
@@ -16,14 +14,14 @@ data <- data.frame(
   value = c(1159, 795, 377, 345, 266, 120, 74, 602),
   products = c(538, 250, -75, -301, 227, 90, 40, 269),
   services = c(621, 545, -302, -44, 39, 30, 34, 333),
-  triangles = c(600,600,-302, 600,600,30,600,600)
+  triangles = c(600, 600, -302, 600, 600, 30, 600, 600)
 )
 groups <- c("products")
-series <- c("triangles","products", "services")
+series <- c("triangles", "products", "services")
 styles <- c(rep('actual', 6), 'forecast', 'actual')
 
 df_styles <- data.frame(
-  products = c(rep('plan',8)),
+  products = c(rep('plan', 8)),
   services = c(rep('actual', 8)),
   triangles = c(rep('plan', 8))
 )
@@ -36,6 +34,18 @@ df <- data.frame(
   wounded = c(1, 8, 5)
 )
 srs <- c("hungry", "relaxed", "wounded")
+
+df_waterfall <- data.frame(
+  'category' = c(
+    "Sales",
+    "Other income",
+    "Personnel expenses",
+    "Material expenses",
+    "Capital expenses",
+    "Investment income"
+  ),
+  'values' = c(12.8, 1.4, -4.2, -8.5, -3.1, 0.6)
+)
 
 test_that("Basic barchart is working", {
   expect_magick(barchart_plot(data, data$city, groups, groups, styles = styles) %>% show())
@@ -57,7 +67,6 @@ test_that("Normalized horizontal barcharts work", {
   groups2 <- c(groups, 'services')
   expect_magick(barchart_plot_normalized(data, data$city, groups2, groups2) %>% show())
 })
-skip('empty')
 
 test_that("Grouped horizontal barcharts work", {
   expect_magick(
@@ -65,7 +74,13 @@ test_that("Grouped horizontal barcharts work", {
   )
 })
 
-test_that("", {
-  expect_magick()
+test_that("Waterfall horizontal barchart works", {
+  expect_magick(
+    barchart_plot_waterfall(
+      df_waterfall$category,
+      df_waterfall$values,
+      add_result = TRUE,
+      result_title = "Profit before tax"
+    ) %>% show()
+  )
 })
-
