abdf9ac5e16a67c6f26afac649a35b26017709af
SawickiBartosz <b.sawicki@student.mini.pw.edu.pl>
2021-07-20 14:20:20 +0200

    add first test and function skeleton

---
+++ R/parse_time_series.R
@@ -0,0 +1,3 @@
+parse_time_series <- function(df, x, y){
+
+}
---
+++ tests/testthat/test-parse_time_series.R
@@ -0,0 +1,15 @@
+context('Time series parsing')
+
+test_that("dummy example works", {
+  df <- data.frame(
+    x = as.Date(c('2021-01-01', '2021-02-01', '2021-03-01')),
+    y = c(10, 12, 15)
+  )
+  result <- parse_time_series(df, x = 'x', y = 'y')
+  expected <- data.frame(
+    x = c(0,0,0),
+    y = c(10,12,15),
+    mon = month.abb[1:3]
+  )
+  expect_equal(result, expected)
+})
