1a1fc1d18d67d66f9753257ba6c2e6e504723aa9
SawickiBartosz <b.sawicki@student.mini.pw.edu.pl>
2021-07-21 12:52:57 +0200

    update time series parsing add different granulation as a param

--- R/parse_time_series.R
+++ R/parse_time_series.R
@@ -1,12 +1,35 @@
-parse_time_series <- function(df, x, y){
+parse_time_series <- function(df, x, y, convert.to = 'months') {
+  stopifnot(convert.to %in% c('weeks', 'months', 'quarters', 'years'))
   x <- df[[x]]
   result.y <- df[[y]]
-  mon.lengths <- lubridate::days_in_month(x)
-  result.months <- names(mon.lengths)
-  result.x <- lubridate::day(x) / mon.lengths * 100
+
+  if (convert.to == 'weeks') {
+    week.lengths <- 7
+    result.categories <- paste(lubridate::epiweek(x), lubridate::year(x), sep = "\n")
+    result.x <- lubridate::wday(x) / week.lengths * 100
+
+  } else if (convert.to == 'months') {
+    mon.lengths <-lubridate::days_in_month(x)
+    result.categories <-  paste(names(mon.lengths), lubridate::year(x), sep = "\n")
+    result.x <- lubridate::day(x) / mon.lengths * 100
+
+  } else if (convert.to == 'quarters') {
+    quarter.lengths <- 92 # TODO get exact quarters length
+    result.categories <- paste(quarter_abbr(x), lubridate::year(x), sep = "\n")
+    result.x <- lubridate::qday(x) / quarter.lengths * 100
+  } else if (convert.to == 'years') {
+    year.lengths <- ifelse(lubridate::leap_year(x), 366, 365)
+    result.categories <- lubridate::year(x)
+    result.x <- lubridate::yday(x) / year.lengths * 100
+  }
+
   result <- data.frame(x = result.x,
                        y = result.y,
-                       mon = result.months)
+                       cat = result.categories)
   row.names(result) <- NULL
   return(result)
+}
+
+quarter_abbr <- function(x){
+  paste0('Q', lubridate::quarter(x))
 }
--- tests/testthat/test-parse_time_series.R
+++ tests/testthat/test-parse_time_series.R
@@ -1,6 +1,6 @@
 context('Time series parsing')
 
-test_that("dummy example works", {
+test_that("dummy months example works", {
   df <- data.frame(
     x = as.Date(c('2021-01-01', '2021-02-01', '2021-03-01')),
     y = c(10, 12, 15)
@@ -9,7 +9,21 @@ test_that("dummy example works", {
   expected <- data.frame(
     x = c(3,3,3),
     y = c(10,12,15),
-    mon = month.abb[1:3]
+    cat = paste(month.abb[1:3], '2021', sep = '\n')
   )
   expect_equal(result, expected, tolerance = 0.12)
+})
+
+test_that('parsing into weeks works', {
+  df <- data.frame(
+    x = as.Date(c('2021-07-19', '2021-07-18', '2021-07-24')),
+    y = c(10, 21, 15)
+  )
+  result <- parse_time_series(df, x = 'x', y = 'y', convert.to = 'weeks')
+  expected <- data.frame(
+    x = c(2/7, 1/7, 1) * 100,
+    y = c(10, 21, 15),
+    cat = rep(paste(29,2021, sep = '\n'),3)
+    )
+  expect_equal(result, expected)
 })
