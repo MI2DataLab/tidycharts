107074d69db12580deb36e4f1eab43d0c624cdc8
SawickiBartosz <b.sawicki@student.mini.pw.edu.pl>
2021-08-05 14:37:39 +0200

    add join plots implementation

--- R/join_plots.R
+++ R/join_plots.R
@@ -1,4 +1,4 @@
-#' Join SVG plots.
+#' Join SVG plots. This function first populates each row in the first column, then rows in the seccond column
 #'
 #' @param ... mumltiple character vectors with SVG content
 #' @param nrows number of rows of plots in joint plot, default is set to number of plots
@@ -17,24 +17,44 @@
 #'   column_chart(df, x = 'mon', series = 'values'),
 #'   column_chart(df, x = 'mon', series = 'values')
 #' ) %>% SVGrenderer()
-join_plots <- function(..., nrows = length(...), nclos = 1){
-  plots <- list(...)
-  widths <- sapply(plots, function(svg_string) get_svg_size(svg_string)[1])
-  heights <- sapply(plots, function(svg_string) get_svg_size(svg_string)[2])
+join_plots <- function(..., nrows = length(list(...)), ncols = 1){
 
-  result_string <- initialize(height = sum(heights), width = max(widths))
-  for(i in 1:length(plots)){
-    result_string <- paste(
-      result_string,
-      transpose_svg(plots[i],
-                    0, cumsum(heights)[i] - heights[i])
-    )
+  n_plots <-length(list(...))
+
+  # check if there are enough rows and cols to show all plots
+  stopifnot(n_plots <= nrows * ncols)
+
+  plots <- array(data = list(...), dim = c(nrows, ncols))
+  widths <- matrix(apply(plots, c(1,2), function(svg_string) get_svg_size(svg_string)[1]), nrow = nrows, ncol = ncols)
+  heights <- matrix(apply(plots, c(1,2), function(svg_string) get_svg_size(svg_string)[2]), nrow = nrows, ncol = ncols)
+  cumulated_widths <- matrix(apply(widths, 1, cumsum), nrow = nrows, ncol = ncols, byrow = T)
+  cumulated_heights <- matrix(apply(heights, 2, cumsum), nrow = nrows, ncol = ncols)
+
+  joint_height <- max(colSums(heights))
+
+  joint_width <- max(rowSums(widths))
+
+  result_string <- initialize(height = joint_height, width = joint_width)
+
+  for(j in 1:ncols){
+    for(i in 1:nrows){
+      if ((j-1) * nrows  + i > n_plots) {
+        break()
+      }
+      translate_x <- cumulated_widths[i,j] - widths[i,j]
+      translate_y <- cumulated_heights[i,j] - heights[i,j]
+      result_string <- paste(
+        result_string,
+        translate_svg(plots[i,j],
+                      translate_x, translate_y)
+      )
+    }
   }
   result_string <- finalize(result_string)
   return(result_string)
 }
 
-transpose_svg  <- function(svg_string, x, y){
+translate_svg<- function(svg_string, x, y){
   return(paste('<g transform="translate(',
         x,
         ' ',
--- tests/testthat/test_joining.R
+++ tests/testthat/test_joining.R
@@ -13,3 +13,44 @@ test_that("Simple joining works",{
     ) %>% show()
   )
 })
+
+
+
+test_that("Simple 2x2 joining works",{
+  set.seed(123)
+  df <- data.frame(
+    mon = month.abb[1:6],
+    values = rnorm(6)
+  )
+  expect_magick(
+    join_plots(
+      column_chart(df, x = 'mon', series = 'values'),
+      column_chart(df, x = 'mon', series = 'values'),
+      column_chart(df, x = 'mon', series = 'values'),
+      column_chart(df, x = 'mon', series = 'values'),
+      nrows = 2,
+      ncols = 2
+    ) %>% show()
+  )
+})
+
+
+test_that("Simple 3x2 joining works",{
+  set.seed(123)
+  df <- data.frame(
+    mon = month.abb[1:6],
+    values = rnorm(6)
+  )
+  expect_magick(
+    join_plots(
+      column_chart(df, x = 'mon', series = 'values'),
+      column_chart(df, x = 'mon', series = 'values'),
+      column_chart(df, x = 'mon', series = 'values'),
+      column_chart(df, x = 'mon', series = 'values'),
+      column_chart(df, x = 'mon', series = 'values'),
+      column_chart(df, x = 'mon', series = 'values'),
+      nrows = 3,
+      ncols = 2
+    ) %>% show()
+  )
+})
