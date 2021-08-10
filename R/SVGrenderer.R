9418b625ae03de44905e75d1464e5ec9306e96a2
SawickiBartosz <b.sawicki@student.mini.pw.edu.pl>
2021-08-10 15:23:50 +0200

    delete SVGrenderer shiny bindings

--- R/SVGrenderer.R
+++ R/SVGrenderer.R
@@ -26,31 +26,3 @@ SVGrenderer <- function(svg_string, width = NULL, height = NULL, elementId = NUL
     elementId = elementId
   )
 }
-
-#' Shiny bindings for SVGrenderer
-#'
-#' Output and render functions for using SVGrenderer within Shiny
-#' applications and interactive Rmd documents.
-#'
-#' @param outputId output variable to read from
-#' @param width,height Must be a valid CSS unit (like \code{'100\%'},
-#'   \code{'400px'}, \code{'auto'}) or a number, which will be coerced to a
-#'   string and have \code{'px'} appended.
-#' @param expr An expression that generates a SVGrenderer
-#' @param env The environment in which to evaluate \code{expr}.
-#' @param quoted Is \code{expr} a quoted expression (with \code{quote()})? This
-#'   is useful if you want to save an expression in a variable.
-#'
-#' @name SVGrenderer-shiny
-#'
-#' @export
-SVGrendererOutput <- function(outputId, width = '100%', height = '400px'){
-  htmlwidgets::shinyWidgetOutput(outputId, 'SVGrenderer', width, height, package = 'SVGrenderer')
-}
-
-#' @rdname SVGrenderer-shiny
-#' @export
-renderSVGrenderer <- function(expr, env = parent.frame(), quoted = FALSE) {
-  if (!quoted) { expr <- substitute(expr) } # force quoted
-  htmlwidgets::shinyRenderWidget(expr, SVGrendererOutput, env, quoted = TRUE)
-}
