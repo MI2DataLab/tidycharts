4c277164c5b1be011248c2bb6a7680801fa22511
SawickiBartosz <b.sawicki@student.mini.pw.edu.pl>
2021-08-10 15:48:29 +0200

    add test for utils and env functions

--- tests/testthat/test-utils.R
+++ tests/testthat/test-utils.R
@@ -1,9 +1,40 @@
 context("Utils/env functions")
 
 test_that('SVGrenderer works', {
-  expect_is(
-    initialize() %>%
-      finalize() %>%
-      SVGrenderer(),
-    "htmlwidget")
+  expect_is(initialize() %>%
+              finalize() %>%
+              SVGrenderer(),
+            "htmlwidget")
+})
+
+test_that('Setting colors works', {
+  expect_error(set_colors(data.frame(
+    'bar_colors' = rep('white', 5),
+    'text_colors' = rep('black', 3)
+  )))
+
+  expect_error(set_colors(data.frame(
+    'bar_colors' = rep('white', 6),
+    'other column name' = rep('black', 6)
+  )))
+
+  expect_silent(set_colors(data.frame(
+    'bar_colors' = rep('white', 6),
+    'text_colors' = rep('black', 6)
+  )))
+})
+
+test_that('Setting styles works',{
+  expect_error(set_styles(
+    data.frame(
+      'bleble' = rep('white', 6),
+      'oink' = rep('black',6)
+    )
+  ))
+  expect_silent(set_styles(
+    data.frame(
+      'stroke' = rep('white', 6),
+      'fill' = rep('black',6)
+    )
+  ))
 })
