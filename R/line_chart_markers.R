b4333afb9070bfa110e3247f3bd023b596804530
ARUMHC <56126275+ARUMHC@users.noreply.github.com>
2021-08-11 16:03:17 +0200

    changing file names

---
+++ R/line_chart_markers.R
@@ -0,0 +1,225 @@
+
+
+#adding a rectangle marker with label above or under + categories labels
+add_point <- function(shift, data, x, value, x_pos, height_of_one, color, k, minimal, cat_width, style = NULL){ #cat jest calym wektore, k to numer serii w ktorej jestesmy
+
+  label<-""
+  if(value > 0){
+    rect <- draw_rect(x_pos-5.6, (244.4-(height_of_one*value)), color, 11.2, 11.2, style = style)
+    y_label <- (250-(height_of_one*value))
+    }
+  else{
+    rect <- draw_rect(x_pos-5.6, (250-5.6 +(height_of_one*abs(value))), color, 11.2, 11.2, style = style)
+    y_label <- (250 + (height_of_one*abs(value)))
+    }
+
+  if(minimal==1){ #ostatni podpis pod spodem
+    #label under
+    label <- add_label(x_pos, y_label + 5.6 + 4.8 +9, value, color) #?
+  }else{
+    #label above
+    label <- add_label(x_pos, y_label -5.6 - 4.8, value, color)
+  }
+
+  return(paste(
+      #marker
+      rect,
+      #label with the marker value
+      label,
+      sep="\n"
+
+    ))
+
+}
+
+find_height <- function(data, series){
+  maxes <- c()
+  for(k in 1:(length(series))){
+    maxes <- c(maxes, max(data[,series[k]]))
+  }
+  maximum <- max(maxes)
+  height_of_one <- 200/maximum
+  return(height_of_one)
+}
+
+
+#----
+draw_points <- function(svg_string, data, x, series, series_labels, cat_width, styles = NULL, height_of_one, min_avg, shift){
+
+  points <- svg_string
+  labels <- ""
+  x_pos <- 80
+
+  for(k in 1:(length(series))){ #going through series
+
+    if(mean(data[,series[k]]) == min_avg){
+      minimal <- 1
+    }else{
+      minimal<-0
+    }
+
+    #color <- colors[k]
+    color <- get_gray_color_stacked(k)$bar_color
+    values <- data[, series[k]]
+    labels <- paste(labels, add_label(80 - 4.8 - 5.6, 250- height_of_one*values[1]+3, series_labels[k], anchor="end"), sep='\n')
+    for(i in 1:(length(x)-1)){ #drawing each point
+      style <- styles[i, k]
+      if (k == 1) {
+        points <- paste(points,
+                        #category label
+                        add_label(x_pos, 268.4 + shift, x[i], "black"),
+                        #ticks
+                        draw_line(x_pos, x_pos, 250,251.6),
+                        #x-axis line
+                        draw_line(x_pos - cat_width/2, x_pos + cat_width/2, 250, 250),
+                        sep = '\n')
+      }
+      points <- paste(points,
+                      add_point(shift, data, x[i], values[i], x_pos, height_of_one, color,k, minimal, cat_width, style = style),
+                      #line between two points
+                      draw_line(x_pos+5.6, x_pos + cat_width, (250-(height_of_one*values[i])), (250-(height_of_one*values[i+1])),color),
+                      sep='\n')
+
+      x_pos <- x_pos + cat_width
+    }
+    i <- length(x)
+    style <- tail(styles, n=1)[[k]]
+    if(k == 1){
+      points <- paste(points,
+                      #category label
+                      add_label(x_pos, 268.4 + shift, x[i], "black"),
+                      #ticks
+                      draw_line(x_pos, x_pos, 250,251.6),
+                      #x-axis line
+                      draw_line(x_pos - cat_width/2, x_pos + cat_width/2, 250, 250),
+                      sep = "\n")
+    }
+    points <- paste(points,
+                    add_point(shift, data, x[i], values[i], x_pos, height_of_one, color, k, minimal, cat_width, style = style ),
+                    sep='\n')
+    x_pos <-80
+  }
+
+
+  return (paste(points, labels, sep='\n'))
+}
+
+#----
+
+#line with few data points
+#' Generates line plot with markers on every value.
+#'
+#' @param data data frame containing data to be plotted
+#' @param cat vector cointaining time interwals of the values
+#' @param series vector containing names of columns in data with values to plot
+#' @param series_labels vector containing names of series to be shown on the plot
+#' @param interval intervals on x axis. The width of the bars depends on this parameter
+#' @param styles optional data frame with style names. Styles of the markers will be plotted accordingly.
+#'
+#' @return SVG string containing chart
+#' @export
+#'
+#' @examples
+#'
+#' #preparing a data frame
+#' data <- data.frame(
+#' time = c("Jan", "Feb", "Mar", "Apr", "May", "Jun"),
+#' PL = (c(51, 42, 50, 58, 78, 79) - 30),
+#' AC = (c(62, 70, 67, 77, 63, 62) - 30)
+#' )
+#' #preparing the styles data frame
+#' styles <- data.frame(
+#'  PL = c("plan", "plan", "plan", "plan", "plan", "plan"),
+#'  AC = c("actual", "actual", "actual", "forecast", "forecast", "forecast")
+#' )
+#'
+#' #generating svg string
+#' line_chart <- line_chart_markers(data, data$time, c("PL", "AC"), c("PL", "AC"),"months", styles)
+#'
+#' #show the plot
+#' line_chart %>% SVGrenderer()
+#'
+#'
+line_chart_markers <- function(data, x, series, series_labels, interval="months", styles = NULL){ #interval <- week, month, quarter, year
+
+  if(length(x) == 1){
+    x <- data[,x]
+  }
+  cat_width <- get_interval_width(interval)$category_width
+  averages <-rowMeans(data[,series])
+  maximum <- max(abs(data[, series]))
+  neg <- data[, series][data[,series] < 0]
+  min_avg <- min(averages)
+  height_of_one <- 200/maximum
+
+  #calculating the shift
+  if(length(neg) == 0){shift <- 0}
+  else{shift <- height_of_one*abs(min(neg)) + 12 + 4.8}
+
+  initialize(width = 80+ cat_width*length(x) + 80, height = 250 + shift + 20) %>%
+    draw_points(.,data, x, series, series_labels, cat_width, styles, height_of_one, min_avg, shift) %>%
+    finalize()
+}
+
+
+#' Generates line plot with markers on every value with index on a given value.
+#'
+#' @param data data frame containing data to be plotted
+#' @param cat vector containing time intervals of the values
+#' @param series vector containing names of columns in data with values to plot
+#' @param series_labels vector containing names of series to be shown on the plot
+#' @param interval intervals on x axis. The width of the bars depends on this parameter
+#' @param index_val numeric value of the index
+#' @param index_label string defining a text that should be displayed in the referencing line. Set by default to index_val.
+#' @param interval intervals on x axis. The width of the bars depends on this parameter
+#' @param styles optional data frame with style names. Styles of the markers will be plotted accordingly.
+#'
+#' @return SVG string containing chart
+#' @export
+#'
+#' @examples
+#'
+#' #preparing a data frame
+#' data <- data.frame(
+#' time = c("Jan", "Feb", "Mar", "Apr", "May", "Jun"),
+#' PL = (c(51, 42, 50, 58, 78, 79) - 30),
+#' AC = (c(62, 70, 67, 77, 63, 62) - 30)
+#' )
+#' #preparing the styles data frame
+#' styles <- data.frame(
+#'  PL = c("plan", "plan", "plan", "plan", "plan", "plan"),
+#'  AC = c("actual", "actual", "actual", "forecast", "forecast", "forecast")
+#' )
+#'
+#' #generating svg string
+#' line_chart_ref <- line_chart_markers_reference(data, data$time, c("PL", "AC"), c("PL", "AC"),42 , "index", styles=styles)
+#'
+#' #show the plot
+#' line_chart_ref %>% SVGrenderer()
+#'
+line_chart_markers_reference <- function(data, x, series, series_labels, ref_val, ref_label=ref_val, interval = "months", styles=NULL){
+
+  if(length(x) == 1){
+    x <- data[,x]
+  }
+  cat_width <- get_interval_width(interval)$category_width
+  averages <-rowMeans(data[,series])
+  maximum <- max(abs(data[, series]))
+  neg <- data[, series][data[,series] < 0]
+  min_avg <- min(averages)
+  height_of_one <- 200/maximum
+
+  #calculating the shift
+  if(length(neg) == 0){shift <- 0}
+  else{shift <- height_of_one*abs(min(neg)) + 12 + 4.8}
+
+  initialize(width = 80 + cat_width*length(x) + 80, height = 250+shift + 20) %>%
+    draw_points(.,data, x, series, series_labels, cat_width, styles, height_of_one, min_avg, shift) %>%
+    paste(.,
+          add_index(80 +cat_width/2 + cat_width*(length(x)-1), 250-height_of_one*ref_val, ref_label),
+          sep='\n') %>%
+    finalize()
+
+}
+
+
---
+++ R/line_chart_normalized.R
@@ -0,0 +1,148 @@
+
+#---
+
+add_marker_normalized <- function(data, cat, value, x, height_of_one, k, y, show_label,if_rect, cat_width){ #cat jest calym wektore, k to numer serii w ktorej jestesmy y - przesuniecie
+
+  #we do not draw the rectangle markers on the top
+  if(if_rect==1){rect<-draw_rect(x-2.4, (247.6-(height_of_one*value))-y, "rgb(127,127,127)", 4.8, 4.8)}
+  else(rect<-"")
+
+  # if(is.na(show_label) == FALSE){
+  #value_label <- add_label(x, 250-y - (height_of_one*value/2) +6, value,get_gray_color_stacked(k)$text_color)
+  if(is.na(show_label) == FALSE){
+    value_label <- add_label(x, 250-y - (height_of_one*value/2) +6, value, get_gray_color_stacked(k)$text_color )
+  }else{
+    value_label<-""
+  }
+  # }else{
+  #   value_label<-""
+  # }
+
+  return(paste(rect,
+    #asisting line
+    draw_line(x, x, 50,250, "white", 0.1),
+    #value label
+    #add_label(x,250-y - (height_of_one*value/2) +6, value,"white"),
+    value_label,
+    sep="\n"
+
+  ))
+
+}
+
+#----
+
+draw_polygons_normalized <- function(svg_string, data, cat, series, series_labels, show_labels, cat_width){
+
+  polygons <- svg_string
+  x = 80
+  labels <- ""
+  #markers need to be separated so the polygons wont overlap them
+  markers <- ""
+  all_sums <- rowSums(data[,series])
+  y <- rep(0, length(cat))
+  height_of_one <- 200/all_sums[1]
+  #-----
+  for(k in 1:(length(series))){ #going through series
+    color <- get_gray_color_stacked(k)$bar_color
+    values <- data[, series[k]]
+    #series labels
+    labels <- paste(labels,
+                    add_label(75.2, 250-y[1] - (height_of_one*values[1]/2), series_labels[k], anchor="end"),
+                    sep="\n"
+
+    )
+
+    for(i in 1:(length(cat)-1)){ #going throug categories
+      #defining if its the top level or not, deciding if the markers are gonna show
+      if(k == length(series)){
+        if_rect <- 0
+      }else{if_rect<-1}
+      if (k == 1) {
+        polygons <- paste(polygons,
+                          #adding ticks
+                          draw_line(x, x ,250, 251.6),
+                          #x-axis line
+                          draw_line(x - cat_width/2, x + cat_width/2, 250, 250),
+                          #category label
+                          add_label(x, 268.4, cat[i]),
+                          sep = '\n')
+      }
+      height_of_one <- 200/all_sums[i]
+      polygons <- paste(polygons,
+                        draw_quadrangle(x, (250-(height_of_one*values[i])) - y[i],
+                                        x + cat_width, (250-(200/all_sums[i+1]*values[i+1])) - y[i+1],
+                                        x + cat_width, 250 - y[i+1],
+                                        x, 250 - y[i],
+                                        color),
+                        sep='\n')
+
+      markers <- paste(markers, add_marker_normalized(data, cat[i], values[i], x, height_of_one, k, y[i], show_labels[i], if_rect, cat_width), sep='\n')
+      x <- x + cat_width
+      y[i] <- y[i] + height_of_one*values[i]
+    }
+    j <- length(cat)
+    height_of_one <- 200/all_sums[j]
+    if (k == 1) {
+      polygons <- paste(polygons,
+                        #adding ticks
+                        draw_line(x, x ,250, 251.6),
+                        #x-axis line
+                        draw_line(x - cat_width/2, x + cat_width/2, 250, 250),
+                        #category label
+                        add_label(x, 268.4, cat[j]),
+                        sep = '\n')
+    }
+    polygons <- paste(polygons,
+                      add_marker_normalized(data, cat[j], values[j], x, height_of_one, j, y[j], show_labels[j], if_rect, cat_width),
+                      sep='\n')
+    x <- 80
+    y[j] <- y[j] + height_of_one*values[j]
+
+  }
+  x <- 80
+  return (paste(polygons, markers,labels, add_index(80 + cat_width*(length(cat)-1), 50),  sep='\n'))
+}
+
+#----
+#' Generates normalized areas (stacked lines) plot. If more than one series is supplied, stacked areas plot is generated.
+#'
+#' @param data data frame containing data to be plotted
+#' @param cat vector cointaining time interwals of the values
+#' @param series vector containing names of columns in data with values to plot
+#' @param series_labels vector containing names of series to be shown on the plot
+#' @param show_labels vector of the same length as cat containg NA or not NA values defining which categories should have labels of values displayed
+#' @param interval intervals on x axis. The width of the bars depends on this parameter
+#'
+#'
+#' @return SVG SVG string containing chart
+#' @export
+#'
+#' @examples
+#'
+#'
+#' #preparing data frames
+#' data <- data.frame(
+#' weeks =    c(28,   29, 30,  31,  32,  33,  34,  35,  36, 37),
+#' Services = c(130,150, 182, 170, 170, 140, 130, 130, 135, 140),
+#' Software = c(100, 88, 83,   90, 92,   95, 129, 130, 130, 135),
+#' Products = c(20,  35, 36,    40, 22,  25, 24,   19,  36,  40)
+#')
+#'
+#'#defining the rest of the arguments
+#' series <- c("Software", "Services", "Products")
+#' labels <- c(NA, 1, NA, 1, NA, NA, 1, NA, 1, NA)
+#'
+#' #generating the SVG string
+#' line_plot_normalized <- line_plot_normalized(data, data$weeks, series, series, labels, "weeks")
+#'
+#' #show the plot
+#' line_plot_normalized %>% SVGrenderer()
+line_plot_normalized <- function( data, cat, series, series_labels, show_labels, interval="months"){
+
+  cat_width <- get_interval_width(interval)$category_width
+  initialize() %>%
+    draw_polygons_normalized(.,data, cat, series, series_labels,show_labels, cat_width) %>%
+    finalize()
+}
+
